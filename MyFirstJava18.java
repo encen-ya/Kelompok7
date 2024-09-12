a82b885d85ef979d16ba83930846b208b6679359
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2024-09-12 14:08:10 +0700

    Biodata Muhammad Febriansyah/18

---
+++ MyFirstJava18.java
@@ -0,0 +1,20 @@
+/**
+ * MyFirstJava11
+ */
+public class MyFirstJava18 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");    
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("Nama    : Muhammad Febriansyah");    
+        System.out.println("NIM     : 244107020199");    
+        System.out.println("Kelas   : 1G");    
+        System.out.println("No Absen: 18");    
+        System.out.println("Prodi   : Teknik Informatika");    
+        System.out.println("Alamat  : Jl Gading Pesantren No 14 ");    
+        System.out.println("No HP   : 081336738076");    
+        System.out.println("Hobi    : Mendengarkan musik");    
+    }
+}
