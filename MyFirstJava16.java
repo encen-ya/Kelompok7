7c7104f5d1598f8ca913c788cbed85085fbcce31
ibrqp <mibrahimalamin98@gmail.com>
2024-09-12 13:50:45 +0700

    Biodata Muchammad Ibrahim Al Amin/16

---
+++ MyFirstJava16.java
@@ -0,0 +1,20 @@
+/**
+ * MyFirstJava11
+ */
+public class MyFirstJava16 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");    
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("Nama    : Muchammad Ibrahim Al Amin");    
+        System.out.println("NIM     : 244107020062");    
+        System.out.println("Kelas   : 1G");    
+        System.out.println("No Absen: 16");    
+        System.out.println("Prodi   : Teknik Informatika");    
+        System.out.println("Alamat  : Cangkringmalang Tengah ");    
+        System.out.println("No HP   : 085784818488");    
+        System.out.println("Hobi    : Olahraga");    
+    }
+}
